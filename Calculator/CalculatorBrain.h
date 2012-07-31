71dcb0c1467ca3a6ffab57028e45b3b344dc6f65
Tom K <krajeec@gmail.com>
2012-07-31 16:57:56 +0200

    finish Walkthrough

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Tom Kraina on 31.07.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
