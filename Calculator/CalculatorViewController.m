d2abf4418d3c68b2139be270ed718f4892e934de
Tom K <krajeec@gmail.com>
2012-08-01 14:52:54 +0200

    update licence

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -3,7 +3,7 @@
 //  Calculator
 //
 //  Created by Tom Kraina on 31.07.2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Tom Kraina. All rights reserved.
 //
 
 #import "CalculatorViewController.h"
