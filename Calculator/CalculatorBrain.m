203f1ece05ba8012012f9ca1e493a1da4e0e6770
Tom K <krajeec@gmail.com>
2012-08-01 18:04:19 +0200

    Finish implementation of runProgram: by implementing popOperandOffStack:

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -25,7 +25,7 @@
 
 - (id)program
 {
-    return [self.program copy];
+    return [self.programStack copy];
 }
 
 - (NSString *)description
@@ -35,7 +35,7 @@
 
 - (void)pushOperand:(double)operand
 {
-    [self.program addObject:[NSNumber numberWithDouble:operand]];
+    [self.programStack addObject:[NSNumber numberWithDouble:operand]];
 }
 
 
@@ -54,6 +54,55 @@
 {
     double result = 0;
 
+    id topOfStack = [stack lastObject];
+    if (topOfStack) {
+        [stack removeLastObject];
+    }
+    
+    if ([topOfStack isKindOfClass:[NSNumber class]]) {
+        result = [topOfStack doubleValue];
+    }
+    else if ([topOfStack isKindOfClass:[NSString class]]) {
+        NSString *operation = topOfStack;
+        if ([operation isEqualToString:@"+"]) {
+            result = [self popOperandOffStack:stack] + [self popOperandOffStack:stack];
+        }
+        else if ([operation isEqualToString:@"*"]) {
+            result = [self popOperandOffStack:stack] * [self popOperandOffStack:stack];
+        }
+        else if ([operation isEqualToString:@"-"]) {
+            double subtrahend = [self popOperandOffStack:stack];
+            result = [self popOperandOffStack:stack] - subtrahend;
+        }
+        else if ([operation isEqualToString:@"/"]) {
+            double divisor = [self popOperandOffStack:stack];
+            if (divisor) {
+                result = [self popOperandOffStack:stack] / divisor;
+            }
+        }
+        else if ([operation isEqualToString:@"sin"]) {
+            result = sin([self popOperandOffStack:stack]);
+        }
+        else if ([operation isEqualToString:@"cos"]) {
+            result = cos([self popOperandOffStack:stack]);
+        }
+        else if ([operation isEqualToString:@"sqrt"]) {
+            result = sqrt([self popOperandOffStack:stack]);
+        }
+        else if ([operation isEqualToString:@"π"]) {
+            result = M_PI;
+        }
+        else if ([operation isEqualToString:@"e"]) {
+            result = M_E;
+        }
+        else if ([operation isEqualToString:@"log"]) {
+            result = log([self popOperandOffStack:stack]);
+        }
+        else if ([operation isEqualToString:@"+/-"]) {
+            result = [self popOperandOffStack:stack] * -1;
+        }
+    }
+    
     return result;
 }
 
@@ -65,53 +114,6 @@
     }
     return [self popOperandOffStack:stack];
 }
-
-/*    
-    double result = 0;
-    
-    if ([operation isEqualToString:@"+"]) {
-        result = [self popOperand] + [self popOperand];
-    }
-    else if ([operation isEqualToString:@"*"]) {
-        result = [self popOperand] * [self popOperand];
-    }
-    else if ([operation isEqualToString:@"-"]) {
-        double subtrahend = [self popOperand];
-        result = [self popOperand] - subtrahend;
-    }
-    else if ([operation isEqualToString:@"/"]) {
-        double divisor = [self popOperand];
-        if (divisor) {
-            result = [self popOperand] / divisor;
-        }
-    }
-    else if ([operation isEqualToString:@"sin"]) {
-        result = sin([self popOperand]);
-    }
-    else if ([operation isEqualToString:@"cos"]) {
-        result = cos([self popOperand]);
-    }
-    else if ([operation isEqualToString:@"sqrt"]) {
-        result = sqrt([self popOperand]);
-    }
-    else if ([operation isEqualToString:@"π"]) {
-        result = M_PI;
-    }
-    else if ([operation isEqualToString:@"e"]) {
-        result = M_E;
-    }
-    else if ([operation isEqualToString:@"log"]) {
-        result = log([self popOperand]);
-    }
-    else if ([operation isEqualToString:@"+/-"]) {
-        result = [self popOperand] * -1;
-    }
-    
-    [self pushOperand:result];
-    
-    return result;
-}
-*/
 
 - (void)clearOperands
 {
